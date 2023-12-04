9e5b4938a26afbada48573e7e0afae01e3484a99
MonongahelaCryptidCooperative <monongahelacryptidcooperative@gmail.com>
2023-12-04 18:48:20 -0500

    New

---
+++ extra_code.py
@@ -0,0 +1,14 @@
+from pybricks.parameters import Button
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Remote, Motor
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+async def setup_hub(hub:PrimeHub):
+    """ Need to change the shutdown button to left and right at same time otherwise
+    it will shutdown when the center button is pressed. Currently not possible to do 
+    in the block interface"""
+    hub.system.set_stop_button([Button.LEFT, Button.RIGHT])
+
+async def drive_base_reset(drive_base:DriveBase):
+    drive_base.reset()
